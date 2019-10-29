cd58a18f3d314eb9e1603eeaca2ddfea6630a5b7
Ayona04 <57102984+Ayona04@users.noreply.github.com>
2019-10-29 00:44:08 +0000

    Update cities.py

--- cities.py
+++ cities.py
@@ -1,4 +1,10 @@
 def read_cities(file_name):
+    file = open(file_name,'r')
+    road_map = []
+    for line in file:
+        road_map.append(tuple(line.strip().split('\t')))
+    file.close()
+    return road_map
     """
     Read in the cities from the given `file_name`, and return 
     them as a list of four-tuples: 
@@ -12,6 +18,10 @@ def read_cities(file_name):
     pass
   
 def print_cities(road_map):
+    print_map = []
+    for element in road_map:
+        print_map.append((element[1], (round(float(element[2]), 2), round(float(element[3]), 2))))
+    print (print_map)
     """
     Prints a list of cities, along with their locations. 
     Print only one or two digits after the decimal point.
@@ -19,15 +29,30 @@ def print_cities(road_map):
     pass
 
 def compute_total_distance(road_map):
+    total_distance = 0.0
+    r = road_map
+    i = -1
+    while i < len(r) - 1: 
+        total_distance += distance(float(r[i][2]), float(r[i][3]), float(r[i + 1][2]), float(r[i + 1][3]))
+        i += 1
+    return total_distance
     """
     Returns, as a floating point number, the sum of the distances of all 
     the connections in the `road_map`. Remember that it's a cycle, so that 
     (for example) in the initial `road_map`, Wyoming connects to Alabama...
     """
+    
     pass
 
 
 def swap_cities(road_map, index1, index2):
+    new_road_map = road_map[:]
+    if index1 != index2:
+        original_index = new_road_map[index1]
+        new_road_map[index1] = new_road_map[index2]
+        new_road_map[index2] = original_index
+    new_total_distance = compute_total_distance(new_road_map)
+    return ((new_road_map, new_total_distance))
     """
     Take the city at location `index` in the `road_map`, and the 
     city at location `index2`, swap their positions in the `road_map`, 
@@ -41,6 +66,7 @@ def swap_cities(road_map, index1, index2):
     pass
 
 def shift_cities(road_map):
+    
     """
     For every index i in the `road_map`, the city at the position i moves
     to the position i+1. The city at the last position moves to the position
@@ -49,6 +75,23 @@ def shift_cities(road_map):
     pass
 
 def find_best_cycle(road_map):
+    shortest_distance = None
+    best_cycle = road_map[:]
+    for n in range(0, 7000):
+        index1 = random.randint(0, len(road_map) - 1)
+        index2 = random.randint(0, len(road_map) - 1)
+        (new_cycle1, distance1) = swap_cities(best_cycle, index1, index2)  
+        if shortest_distance is None or distance1 < shortest_distance:
+            shortest_distance = distance1
+            best_cycle = new_cycle1
+    for n in range(0, 3000):
+        index = random.randint(0, len(road_map) - 1)
+        (new_cycle2, distance2) = swap_adjacent_cities(best_cycle, index)
+        if distance2 < shortest_distance:
+            shortest_distance = distance2
+            best_cycle = new_cycle2
+    return best_cycle
+            
     """
     Using a combination of `swap_cities` and `shift_cities`, 
     try `10000` swaps/shifts, and each time keep the best cycle found so far. 
@@ -58,6 +101,11 @@ def find_best_cycle(road_map):
     pass
 
 def print_map(road_map):
+    road_map = read_cities("city_data.txt")
+    print_cities(road_map)
+    compute_total_distance(road_map)
+    find_best_cycle(road_map)
+    print_map(road_map)
     """
     Prints, in an easily understandable format, the cities and 
     their connections, along with the cost for each connection 
