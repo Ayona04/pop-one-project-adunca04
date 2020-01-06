389377f6411f2f0d3a420ff63b2be41503ea76fe
Ayona04 <57102984+Ayona04@users.noreply.github.com>
2020-01-06 21:04:10 +0000

    Update test_cities.py

--- test_cities.py
+++ test_cities.py
@@ -1,7 +1,6 @@
 import pytest
 from cities import *
 
-
 def test_compute_total_distance():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
                 ("Delaware", "Dover", 39.161921, -75.526755),\
@@ -16,6 +15,8 @@ def test_compute_total_distance():
                                distance( 44.95, -93.094, 38.197274, -84.86311), "should be 38.24 "  
     assert compute_total_distance(road_map1)((9.386+18.496+10.646)) == 38, "should be 38.524 "
     assert compute_total_distance(road_map1)((9.386+18.496+10.646)) == 59.543,"should be 38.524 "
+    assert compute_total_distance(road_map1)((9.386+18.496+10.646)) == 60,"should be 38.524 "
+    assert compute_total_distance(road_map1)((9.386+18.496+44.95+-93.094)) == 38.524,"should be 38.524"
 
 if __name__ == "__main__":
     test_compute_total_distance()
@@ -26,16 +27,21 @@ def test_swap_cities():
                   ('Connecticut', 'Hartford', '41.767', '-72.677'),\
                   ('Delaware', 'Dover', '39.161921', '-75.526755'),\
                   ('Florida', 'Tallahassee', '30.4518', '-84.27277')]
-	new_road_map1 = [('Colorado', 'Denver', '39.7391667', '-104.984167'),\
-	  ('Connecticut', 'Hartford', '41.767', '-72.677'),\
-	  ('Florida', 'Tallahassee', '30.4518', '-84.27277'),\
-	  ('Delaware', 'Dover', '39.161921', '-75.526755')]
+    new_road_map1 = [('Colorado', 'Denver', '39.7391667', '-104.984167'),\
+		     ('Connecticut', 'Hartford', '41.767', '-72.677'),\
+		     ('Florida', 'Tallahassee', '30.4518', '-84.27277'),\
+		     ('Delaware', 'Dover', '39.161921', '-75.526755')]
 	new_total_distance1 = compute_total_distance(new_road_map1)
 	assert (swap_cities(road_map1,1,3)),\
 	(new_road_map1, new_total_distance1)
-    
+	
+    assert (swap_cities
+    assert (swap_cities
+    assert (swap_cities
+    assert (swap_cities
 
 def test_shift_cities():
+	
     '''add your tests'''
 
 
